ba0628ffd213e73942ab35b683b52fdb4bc76574
Kenobi-Knobs <konchatniyvitaliy@gmail.com>
2020-11-22 18:11:52 +0300

    UpdateTest

--- src/test/java/ActivePingTest.java
+++ src/test/java/ActivePingTest.java
@@ -1,18 +1,68 @@
 package test.java;
 
 import main.java.ActivePing;
+import main.java.DBController;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ActivePingTest {
+    ActivePing ap = new ActivePing();
+    String config = new String(Files.readAllBytes(Paths.get("config.txt")));
+    JSONParser parser = new JSONParser();
+    JSONObject jsonConfig = (JSONObject) parser.parse(config);
+
+    public DBController db =new DBController((String) jsonConfig.get("test-user"), (String) jsonConfig.get("test-pass"), (String) jsonConfig.get("test-url"));;
+
+    ActivePingTest() throws ParseException, IOException, SQLException, ClassNotFoundException {}
+
+    public void executeUpdate(DBController db, String query) throws SQLException {
+        PreparedStatement ps = db.getConnection().prepareStatement(query);
+        ps.executeUpdate();
+        ps.close();
+    }
+
+    @BeforeEach
+    public void Before() throws SQLException {
+        executeUpdate(db, "INSERT INTO `PingList` (`id`, `user_id`, `address`, `ping_interval`, `last_ping_time`, `last_code`, `down_timing`, `current_down`) VALUES (NULL, '3', 'https://googled.com', '1', NULL, '200', '1', NULL);");
+        executeUpdate(db, "INSERT INTO `PingList` (`id`, `user_id`, `address`, `ping_interval`, `last_ping_time`, `last_code`, `down_timing`, `current_down`) VALUES (NULL, '2', 'https://googles.com', '1', NULL, '200', '1', NULL);");
+        executeUpdate(db, "INSERT INTO `PingList` (`id`, `user_id`, `address`, `ping_interval`, `last_ping_time`, `last_code`, `down_timing`, `current_down`) VALUES (NULL, '1', 'https://google.com', '1', NULL, '200', '1', NULL);");
+    }
 
     @Test
-    void getPingList() {
+    void getPingList() throws SQLException {
+        ArrayList<String> expArray = new ArrayList<>();
+        expArray.add("https://googled.com|0|1|1");
+        expArray.add("https://googles.com|0|1|1");
+        expArray.add("https://google.com|0|1|1");
+
+        ArrayList<String> actualArray = ap.getPingList(db);
+        ArrayList<String> preparedActualArray = new ArrayList<>();
+        for (String str : actualArray){
+            String[] split = str.split("\\|");
+            preparedActualArray.add(split[1] + "|" + split[2] + "|" + split[3] + "|" + split[4]);
+        }
+
+        assertArrayEquals(expArray.toArray(), preparedActualArray.toArray());
     }
 
     @Test
     void pingList() {
+
     }
 
     @Test
@@ -20,6 +70,11 @@ class ActivePingTest {
         assertEquals(200, ActivePing.ping("http://www.google.com"));
         assertEquals(404, ActivePing.ping("http://wekqwq.com"));
         assertEquals(200, ActivePing.ping("https://www.google.com"));
-        assertEquals(404, ActivePing.ping("https://ewqewq.com"));
+        assertEquals(404, ActivePing.ping("https://wekqwq.com"));
+    }
+
+    @AfterEach
+    public void After() throws SQLException {
+        executeUpdate(db, "DELETE FROM `PingList` WHERE 1");
     }
 }
